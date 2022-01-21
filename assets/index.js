f00b14d9eb6917c6530df31558cd63254ba6a277
Spotexx <travesimo@gmail.com>
2022-01-21 14:43:07 -0500

    pseudo code, css reset and started a nav which will contain the highscore and timer

--- assets/index.js
+++ assets/index.js
@@ -1,1 +1,8 @@
-console.log("connected");
+//Top left: View HighScores button => sends user alert containing high scores
+//Top right: time remaining => loses game if time runs out
+//main area: Code Quiz => User selects answer from multiple choice
+    //if correct => user gets a point
+    //if incorrect => 10 seconds penalty to time remaining
+    //after every question => "correct" or "incorrect" is displayed underneath the question
+    //at the end of the quiz => user can save their initials and score to local storage for highscores
+    //after highscore is saved => user is sent a highscore alert and given the option to play again
